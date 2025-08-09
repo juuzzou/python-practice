d29a1b804f371c2035f904fb20526f3ff886d197
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-09 04:29:53 +0300

    updated file

--- pynative-problems/pynative-function-problems.py
+++ pynative-problems/pynative-function-problems.py
@@ -43,3 +43,19 @@ def show_employee(name, salary=9000):
 show_employee('Wyatt', 6000)
 show_employee('Alfajer')
 
+"""
+    Exercise 5: Create an inner function
+    Create a program with nested functions to perform an addition calculation as follows:
+        Define an outer function that accepts two parameters, a and b.
+        Inside this outer function, define an inner function that calculates the sum of a and b.
+        The outer function should then add 5 to this sum.
+        Finally, the outer function should return the resulting value.
+
+"""
+
+def outerFunction(a, b):
+    def innerFunction():
+        return a + b
+    return innerFunction() + 5
+
+print(outerFunction(5, 10))
