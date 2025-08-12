a83dc0e22b46b3fb7cc7696524be1ccb24c5fe1d
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-12 16:15:23 +0300

    file update

--- pynative-problems/pynative-string-problems.py
+++ pynative-problems/pynative-string-problems.py
@@ -1,4 +1,5 @@
 from math import floor
+from string import ascii_lowercase, ascii_uppercase
 """
     Exercise 1A: Create a string made of the first, middle and last character
     Write a program to create a new string made of an input string’s first, middle, and last character.
@@ -35,3 +36,37 @@ def insertString(string_a, string_b):
     return print(new_string)
 
 insertString("Ault", "Kelly")
+
+"""
+    Exercise 3: Create a new string made of the first, middle, and last characters of each input string
+    Given two strings, s1 and s2, write a program to return a new string made of s1 and s2’s first, middle, and last characters.
+"""
+
+def buildString(string_a, string_b):
+    new_string = string_a[0] + string_b[0] + string_a[floor(len(string_a) / 2)] + string_b[floor(len(string_b) / 2)] + \
+                 string_a[len(string_a) - 1] + string_b[len(string_b) - 1]
+    return print(new_string)
+
+buildString("America", "Japan")
+
+"""
+    Exercise 4: Arrange string characters such that lowercase letters should come first
+    Given string contains a combination of the lower and upper case letters. 
+    Write a program to arrange the characters of a string so that all lowercase letters should come first.
+"""
+
+def arrangeString(input_string):
+    new_string = ''
+    for character in input_string:
+        if character in ascii_lowercase:
+            new_string += character
+        else:
+            continue
+    for character in input_string:
+        if character in ascii_uppercase:
+            new_string += character
+        else:
+            continue
+    return print(new_string)
+
+arrangeString("PyNaTive")
