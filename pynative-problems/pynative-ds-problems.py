a17d2373fd80af516ab9e6edf27047b24aee70a8
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-14 20:13:30 +0300

    file init

--- pynative-problems/pynative-ds-problems.py
+++ pynative-problems/pynative-ds-problems.py
@@ -5,14 +5,11 @@
 """
 def createListOfTwo(list_1, list_2):
     _temp = []
-    odd_list = list_1[1::2]
-    print(f'Odd: {odd_list}')
-    even_list = list_2[::2]
-    print(f'Odd: {even_list}')
-    _temp.extend(odd_list)
-    _temp.extend(even_list)
+    print(f'Odd: {list_1[1::2]}')
+    print(f'Odd: {list_2[::2]}')
+    _temp.extend(list_1[1::2])
+    _temp.extend(list_2[::2])
     return _temp
-
 
 l1 = [3, 6, 9, 12, 15, 18, 21]
 l2 = [4, 8, 12, 16, 20, 24, 28]
