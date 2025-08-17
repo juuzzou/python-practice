6bbaaee45f86941fa2a5ad617f7b9d4e2321462a
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-17 19:19:05 +0300

    file update

--- pynative-problems/pynative-list-problems.py
+++ pynative-problems/pynative-list-problems.py
@@ -7,6 +7,8 @@
     Check if Empty: Write a code to check is list empty.
 
 """
+
+
 def newFunction(list):
     print(f'Initial list: {list}')
     print(f'Third item: {list[2]}')
@@ -16,5 +18,34 @@ def newFunction(list):
     else:
         print(f'List is not empty')
 
+
 my_list = [10, 20, 30, 40, 50]
 newFunction(my_list)
+
+"""
+    Exercise 2: Perform List Manipulation
+    Perform following list manipulation operations on given list:
+    Change Element: Change the second element of a list to 200 and print the updated list.
+    Append Element: Add 600 o the end of a list and print the new list.
+    Insert Element: Insert 300 at the third position (index 2) of a list and print the result.
+    Remove Element (by value): Remove 600 from the list and print the list.
+    Remove Element (by index): Remove the element at index 0 from the list print the list.
+"""
+
+
+def listManipulation(list):
+    print(f'Initial list: {list}')
+    list[1] = 200
+    print(f'After changing second element: {list}')
+    list.append(600)
+    print(f'List after appending 600: {list}')
+    list.insert(2, 300)
+    print(f'List after inserting 300 at index 2: {list}')
+    list.remove(600)
+    print(f'List after removing 600 (by value): {list}')
+    list.pop(0)
+    print(f'List after removing element at index 0: {list}')
+
+
+my_list = [10, 20, 30, 40, 50]
+listManipulation(my_list)
