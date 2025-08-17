d02623fdeb9d76d2278b4fbcd3a7226126047223
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-17 19:46:15 +0300

    file update

--- pynative-problems/pynative-list-problems.py
+++ pynative-problems/pynative-list-problems.py
@@ -49,3 +49,67 @@ def listManipulation(list):
 
 my_list = [10, 20, 30, 40, 50]
 listManipulation(my_list)
+
+"""
+    Exercise 3: Sum and average of all numbers in a list
+    Calculate and print the sum and average of all numbers in a list.
+"""
+
+
+def findSumAndAvg(list):
+    print(f'Sum: {sum(list)}\nAvg: {sum(list) / len(list)}')
+
+
+my_list = [10, 20, 30, 40, 50]
+findSumAndAvg(my_list)
+
+"""
+    Exercise 4: Reverse a list
+"""
+
+
+def reverseList(list):
+    return list[::-1]
+
+
+list1 = [100, 200, 300, 400, 500]
+print(reverseList(list1))
+
+"""
+    Exercise 5: Turn every item of a list into its square
+    Given a list of numbers. write a program to turn every item of a list into its square.
+"""
+
+
+def squareList(list):
+    return [i ** 2 for i in list]
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7]
+print(squareList(numbers))
+
+"""
+    Exercise 6: Find Maximum and Minimum
+    Find and print the largest and smallest number in a list [8, 2, 15, 1, 9].
+"""
+
+
+def findMaxMin(list):
+    print(f'The largest value is {max(list)}\nThe smallest value is {min(list)}')
+
+
+data = [8, 2, 15, 1, 9]
+findMaxMin(data)
+
+"""
+    Exercise 7: Count Occurrences
+    Count and print how many times 'Football' appears in list. 
+"""
+
+
+def findOccurrences(list, word):
+    return list.count(word)
+
+
+sports = ['Cricket', 'Football', 'Hockey', 'Football', 'Tennis']
+print(findOccurrences(sports, 'Football'))
