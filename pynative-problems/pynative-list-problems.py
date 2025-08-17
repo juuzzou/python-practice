37c085608d8bb13c393e691f2d79065496081880
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-17 21:17:07 +0300

    file update

--- pynative-problems/pynative-list-problems.py
+++ pynative-problems/pynative-list-problems.py
@@ -113,3 +113,101 @@ def findOccurrences(list, word):
 
 sports = ['Cricket', 'Football', 'Hockey', 'Football', 'Tennis']
 print(findOccurrences(sports, 'Football'))
+
+"""
+    Exercise 8: Sort a list of numbers
+    Sort a given list of numbers in ascending order and print it.
+"""
+
+
+def sortList(list):
+    print(f'Original list: {list}')
+    list.sort()
+    print(f'Sorted list: {list}')
+
+
+numbers = [5, 2, 8, 1, 9]
+sortList(numbers)
+
+"""
+    Exercise 9: Create a copy of a list
+    Create a copy of a list [10, 20, 30] and modify the copy. Print both the original and the copied list to demonstrate they are independent.
+"""
+
+
+def createCopyList(list):
+    print(f'Original list: {list}')
+    list_2 = list.copy()
+    print(f'Copy of a list: {list_2}')
+
+
+a_certain_list = [10, 20, 30]
+createCopyList(a_certain_list)
+
+"""
+    Exercise 10: Combine two lists
+    Combine given two lists into a single list and print it.
+"""
+
+
+def extendList(list_1, list_2):
+    list_1.extend(list_2)
+    return list_1
+
+
+list_a = [1, 2]
+list_b = [3, 4]
+print(extendList(list_a, list_b))
+
+"""
+    Exercise 11: Remove empty strings from the list of strings
+"""
+
+
+def removeEmptyStrings(list):
+    new_list = []
+    for item in list:
+        if item != '':
+            new_list.append(item)
+        else:
+            continue
+    return new_list
+
+
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+print(removeEmptyStrings(list1))
+
+"""
+    Exercise 12: Remove Duplicates from list
+    Write a function that takes a list with duplicate elements and returns a new list with only unique elements. 
+"""
+
+
+def removeDuplicates(list):
+    list_no_duplicates = []
+    for item in list:
+        if item not in list_no_duplicates:
+            list_no_duplicates.append(item)
+        else:
+            continue
+    return list_no_duplicates
+
+
+list_with_duplicates = [1, 2, 2, 3, 1, 4, 5, 4]
+print(removeDuplicates(list_with_duplicates))
+
+"""
+    Exercise 13: Remove all occurrences of a specific item from a list
+    Given a Python list, write a program to remove all occurrences of item 20.
+"""
+
+
+def removeSpecificValue(list, value):
+    for item in list:
+        if item == value:
+            list.remove(value)
+    return list
+
+
+list1 = [5, 20, 15, 20, 25, 50, 20]
+print(removeSpecificValue(list1, 20))
