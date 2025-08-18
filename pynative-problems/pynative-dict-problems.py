40f0d4a0b0bb58c55b8d5c5f734be48432dcfaca
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-19 00:59:48 +0300

    File update

--- pynative-problems/pynative-dict-problems.py
+++ pynative-problems/pynative-dict-problems.py
@@ -221,4 +221,19 @@ sample_dict = {
 keys = ["name", "salary"]
 print(iReallyDontKnow(keys, sample_dict))
 
+"""
+    Exercise 13: Check if a value exists in a dictionary
+    While we know how to check for a key’s presence in a dictionary, it’s sometimes necessary to determine if a specific value exists.
+    Write a Python program to check if the value 200 is present in the provided dictionary.
+"""
 
+
+def checkIfExists(dictionary, value):
+    if value in dictionary.values():
+        print(f'{value} is in a dictionary')
+    else:
+        print(f"{value} isn't in a dictionary")
+
+
+sample_dict = {'a': 100, 'b': 200, 'c': 300}
+checkIfExists(sample_dict, 200)
