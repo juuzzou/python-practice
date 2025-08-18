8467027473105446d1d9e62e332b7650ebdaba42
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-19 01:38:30 +0300

    File update

--- pynative-problems/pynative-dict-problems.py
+++ pynative-problems/pynative-dict-problems.py
@@ -281,3 +281,15 @@ print(findMin(sample_dict))
     Write a Python program to change Brad’s salary to 8500 in the following dictionary.
 """
 
+
+def stopIt(dictionary):
+    dictionary['emp3']['salary'] = 8500
+    return dictionary
+
+
+sample_dict = {
+    'emp1': {'name': 'Jhon', 'salary': 7500},
+    'emp2': {'name': 'Emma', 'salary': 8000},
+    'emp3': {'name': 'Brad', 'salary': 500}
+}
+print(stopIt(sample_dict))
