caffdc7e323a50f0fce994b8d60acbcfe44a8a43
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-20 13:42:57 +0300

    Initial commit

---
+++ pynative-problems/pynative-set-problems.py
@@ -0,0 +1,7 @@
+"""
+    Exercise 1: Perform Basic Set Operations
+        Create a Set: Create a set named fruits containing “apple”, “banana”, “mango”, and “orange”.
+        Add Element: Add “grape” to the fruits set.
+        Remove Element: Remove “banana” from the fruits set.
+        Discard Element: Try to discard “mango” from the fruits set.
+"""
