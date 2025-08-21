56e3d53ed87caff23cb2c21550651c34d9046a69
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-21 03:44:27 +0300

    File update

--- pynative-problems/pynative-set-problems.py
+++ pynative-problems/pynative-set-problems.py
@@ -61,3 +61,19 @@ def differenceSet(set_1, set_2):
 set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
 print(differenceSet(set1, set2))
+
+"""
+    Exercise 5: Symmetric Difference
+    Find the symmetric difference of set1 and set2. 
+    Write a code to returns a new set containing elements that are unique to either set1 or set2, but not in both. 
+    It’s like finding the union and then removing the intersection.
+"""
+
+
+def symmetricDifferenceSet(set_1, set_2):
+    return set_1 ^ set_2
+
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+print(symmetricDifferenceSet(set1, set2))
