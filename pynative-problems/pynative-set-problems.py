c9bbaaba3465865289e62d79ed11a37a1b8807d5
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-21 03:33:39 +0300

    File update

--- pynative-problems/pynative-set-problems.py
+++ pynative-problems/pynative-set-problems.py
@@ -19,3 +19,18 @@ def setOperations(*args):
 
 
 print(setOperations("apple", "orange", "mango", "banana"))
+
+"""
+    Exercise 2: Union of Sets
+    Find the union of set1 and set2. Write a Python program to return a new set with unique items from both sets by removing duplicates.
+"""
+
+
+def unionSet(set_1, set_2):
+    return set_1 | set_2
+
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+print(unionSet(set1, set2))
+
