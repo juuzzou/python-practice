e9efce9dffec706941427b28defe8a4faa94679b
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-21 03:39:14 +0300

    File update

--- pynative-problems/pynative-set-problems.py
+++ pynative-problems/pynative-set-problems.py
@@ -34,3 +34,16 @@ set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
 print(unionSet(set1, set2))
 
+"""
+    Exercise 3: Intersection of Sets
+    Find the intersection of set1 and set2. Write a code to return a new set containing only the elements that are common to both set1 and set2
+"""
+
+
+def intersectionSet(set_1, set_2):
+    return set_1 & set_2
+
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+print(intersectionSet(set1, set2))
