2d710b53664a67dfa6d75d2174c2c371f79636e2
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-23 02:01:38 +0300

    Initial commit

---
+++ pynative-problems/pynative-tuple-problems.py
@@ -0,0 +1,15 @@
+"""
+    Exercise 1: Perform Basic Tuple Operations
+        Create a Tuple: Create a tuple named my_tuple containing the numbers 1, 2, 3, 4, and 5.
+        Access Elements: Access and print the third element of my_tuple.
+        Tuple Length: Find and print the length of my_tuple.
+"""
+
+
+def basicOperations():
+    new_tuple = (1, 2, 3, 4, 5)
+    print(f'The 3rd element is {new_tuple[2]}')
+    print(f'The length of this tuple is {len(new_tuple)}')
+
+
+basicOperations()
