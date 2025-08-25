62b1a366bf704ef5ef401fa163d8d9079681f207
Wyatt Bosinzon <vkudr03@gmail.com>
2025-08-26 02:29:07 +0300

    File update

---
+++ pynative-problems/pynative-oop-problems/pynative-oop-first.py
@@ -0,0 +1,14 @@
+"""
+    OOP Exercise 1: Create a Class with instance attributes
+    Write a Python program to create a Vehicle class with max_speed and mileage instance attributes.
+"""
+
+
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+
+new_car = Vehicle(160, 5000)
+print(f'My rented has maximum speed of {new_car.max_speed} mi/h and mileage of {new_car.mileage} mi.')
